dafa90c1b3383fc9f2196a36aaf854fd296a46c9
Ali Parsai <ali.parsai@email.com>
2016-09-30 18:04:22 +0200

    clean up...

---
+++ src/main/java/nul/study/videostore/Movie.java
@@ -0,0 +1,32 @@
+package nul.study.videostore;
+
+public abstract class Movie {
+
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	public static final int CHILDRENS = 2;
+
+	private String title;
+	private int priceCode;
+
+	public Movie(String title, int priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int code) {
+		priceCode = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public abstract double determineAmount(int daysRented);
+
+	public abstract int determineFrequentRenterPoints(int daysRented);
+}
